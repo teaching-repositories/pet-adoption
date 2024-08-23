385647daa5da6e5223695cdb7d09e05d0fe6afb8
anotidamalandu <anotidamalandu@gmail.com>
2024-08-24 02:56:54 +0400

    initalized and implemented base class

--- src/pet.py
+++ src/pet.py
@@ -1,6 +1,19 @@
 class Pet:
     def __init__(self, name, age, breed, species):
-        # TODO: Initialize the pet attributes
-        pass
+        self._name = name  # Using encapsulation with a private attribute
+        self._age = age
+        self._breed = breed
+        self._species = species
+        self._adopted = False  # All pets are initially not adopted
 
-    # TODO: Implement other necessary methods
+    def get_details(self):
+        return f"{self._name}, {self._species}, {self._breed}, {self._age} years old, Adopted: {self._adopted}"
+
+    def adopt(self):
+        self._adopted = True
+
+    def is_adopted(self):
+        return self._adopted
+
+    def __str__(self):
+        return self.get_details()
